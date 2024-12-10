ecf4b13d0531c0a4575054c2d067ba5972b6ec89
Atipat D <95071528+Atipat-CMU@users.noreply.github.com>
2024-12-10 20:12:30 +0700

    init react flow

--- features/systems/components/Editor.tsx
+++ features/systems/components/Editor.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { ReactFlow, Controls, Background } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 
 export default function Editor() {
-  return <div>dfkj</div>;
+  return (
+    <div className="h-full">
+      <ReactFlow>
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 }
