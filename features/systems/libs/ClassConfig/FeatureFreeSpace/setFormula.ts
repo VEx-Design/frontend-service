e6051a93ad993e65742f87fbd515152112e4ef68
Atipat D <95071528+Atipat-CMU@users.noreply.github.com>
2025-03-10 09:03:03 +0700

    FIX: free space apply formula abnormally

--- features/systems/libs/ClassConfig/FeatureFreeSpace/setFormula.ts
+++ features/systems/libs/ClassConfig/FeatureFreeSpace/setFormula.ts
@@ -4,9 +4,26 @@ export default function setFormula(
   currentFreeSpace: FreeSpace,
   formulaStatus: FormulaFStatus
 ): FreeSpace {
-  const newFreeSpace = {
-    ...currentFreeSpace,
-    formulas: [formulaStatus.formula],
+  const index = currentFreeSpace.formulas.findIndex(
+    (f) => f.paramId === formulaStatus.formula.paramId
+  );
+
+  const newFreeSpace = { ...currentFreeSpace };
+  const newFormula = {
+    ...formulaStatus.formula,
+    completeStream:
+      formulaStatus.formula.formulaTokens.reduce((acc, token, index) => {
+        acc += token.stream + `x[${index + 1}]`;
+        return acc;
+      }, "") + formulaStatus.formula.lastStream,
+    variables: formulaStatus.formula.formulaTokens.map(
+      (token) => token.variable
+    ),
   };
+  if (index === -1) {
+    newFreeSpace.formulas.push(newFormula);
+  } else {
+    newFreeSpace.formulas[index] = newFormula;
+  }
   return newFreeSpace;
 }
